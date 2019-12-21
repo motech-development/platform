16ced1b5745391a10524f5de4e084c42726ffcc0
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2019-12-21 22:52:02 +0000

    chore(ui): link component
    affects: @motech-development/breeze-ui

---
+++ packages/breeze-ui/src/Link/Link.stories.tsx
@@ -0,0 +1,21 @@
+import { text, withKnobs } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import BaseStyles from '../BaseStyles/BaseStyles';
+import Card from '../Card/Card';
+import Link from './Link';
+
+const stories = storiesOf('Link', module);
+
+stories.addDecorator(withKnobs);
+
+stories.add('Basisc link', () => (
+  <MemoryRouter>
+    <BaseStyles />
+
+    <Card>
+      <Link to={text('To', '/home')}>{text('Text', 'This is a link')}</Link>
+    </Card>
+  </MemoryRouter>
+));
---
+++ packages/breeze-ui/src/Link/__tests__/Link.tsx
@@ -0,0 +1,31 @@
+import { render, RenderResult } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import Link from '../Link';
+
+describe('Link', () => {
+  let component: RenderResult;
+
+  beforeEach(() => {
+    component = render(
+      <MemoryRouter>
+        <Link to="/test">Hello</Link>
+      </MemoryRouter>,
+    );
+  });
+
+  it('should have the correct styles', async () => {
+    const { findByText } = component;
+
+    await expect(findByText('Hello')).resolves.toHaveStyle(`
+      color: #2e9dc8;
+      font-weight: 600;
+    `);
+  });
+
+  it('should link to the correct location', async () => {
+    const { findByText } = component;
+
+    await expect(findByText('Hello')).resolves.toHaveAttribute('href', '/test');
+  });
+});
