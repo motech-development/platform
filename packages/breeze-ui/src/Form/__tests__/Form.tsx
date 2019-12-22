9c17813f8d5ee84feb649f0dcffe40656750c45e
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2019-12-22 00:12:14 +0000

    chore(ui): form component tests
    affects: @motech-development/breeze-ui

---
+++ packages/breeze-ui/src/Form/__tests__/Form.tsx
@@ -0,0 +1,68 @@
+import { act, fireEvent, render, RenderResult } from '@testing-library/react';
+import React from 'react';
+import { object, ObjectSchema, string } from 'yup';
+import TextBox from '../../TextBox/TextBox';
+import Form from '../Form';
+
+interface IInitialValues {
+  test: string;
+}
+
+describe('Form', () => {
+  let component: RenderResult;
+  let initialValues: IInitialValues;
+  let validationSchema: ObjectSchema<IInitialValues>;
+  let onSubmit: jest.Mock;
+
+  beforeEach(() => {
+    onSubmit = jest.fn();
+    initialValues = {
+      test: '',
+    };
+    validationSchema = object().shape({
+      test: string().required(),
+    });
+
+    component = render(
+      <Form
+        submitLabel="Submit"
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <TextBox label="Test" name="test" />
+      </Form>,
+    );
+  });
+
+  it('should disable save button if form is invalid', async () => {
+    const { findByRole } = component;
+
+    await expect(findByRole('button')).resolves.toHaveAttribute('disabled');
+  });
+
+  it('should enable save button if for is valid', async () => {
+    const { findByLabelText, findByRole } = component;
+    const input = await findByLabelText('Test');
+
+    fireEvent.change(input, { target: { value: 'Hello world' } });
+
+    await expect(findByRole('button')).resolves.not.toHaveAttribute('disabled');
+  });
+
+  it('should submit with the correct values', async () => {
+    const { findByLabelText, findByRole } = component;
+
+    await act(async () => {
+      const input = await findByLabelText('Test');
+
+      fireEvent.change(input, { target: { value: 'Hello world' } });
+
+      const button = await findByRole('button');
+
+      fireEvent.click(button);
+    });
+
+    expect(onSubmit).toHaveBeenCalled();
+  });
+});
