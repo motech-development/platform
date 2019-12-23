ad4596c2fd70ee58675060c94dc7a6163b0424d9
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2019-12-23 17:29:01 +0000

    chore(auth): set up testing
    affects: @motech-development/auth, @motech-development/id, @motech-development/breeze-ui

---
+++ applications/auth/src/__tests__/register.ts
@@ -0,0 +1,7 @@
+describe('register', () => {
+  it.todo('should throw if body is incorrect');
+
+  it.todo('should throw if Auth0 returns an error');
+
+  it.todo('should return a successful response when the user is registered');
+});
