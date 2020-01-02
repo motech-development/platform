31be801504a65fd2f4482442994d1bc5e411e5c1
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-01-02 20:12:00 +0000

    chore(auth): rewrite tests
    affects: @motech-development/auth

--- packages/auth/src/__tests__/AuthProvider.tsx
+++ packages/auth/src/__tests__/AuthProvider.tsx
@@ -1,106 +1,232 @@
 import { fireEvent, render } from '@testing-library/react';
 import React, { FC } from 'react';
-import AuthProvider, { AuthContext, useAuth } from '../AuthProvider';
+import AuthProvider, { AuthContext, AuthUser, useAuth } from '../AuthProvider';
 
 const TestComponent: FC = () => {
-  const { loginWithRedirect, logout } = useAuth();
+  const {
+    isAuthenticated,
+    isLoading,
+    loginWithRedirect,
+    logout,
+    user,
+  } = useAuth();
 
   return (
     <>
-      <button type="button" data-testid="log-in" onClick={loginWithRedirect}>
-        Log in
-      </button>
+      {isLoading ? (
+        <div data-testid="loading">Loading...</div>
+      ) : (
+        <>
+          {isAuthenticated ? (
+            <>
+              <div data-testid="user">{user?.name}</div>
 
-      <button
-        type="button"
-        data-testid="log-out"
-        onClick={() =>
-          logout({
-            returnTo: 'somewhere',
-          })
-        }
-      >
-        Log out
-      </button>
+              <button
+                type="button"
+                data-testid="log-out"
+                onClick={() =>
+                  logout({
+                    returnTo: 'somewhere',
+                  })
+                }
+              >
+                Log out
+              </button>
+            </>
+          ) : (
+            <button
+              type="button"
+              data-testid="log-in"
+              onClick={loginWithRedirect}
+            >
+              Log in
+            </button>
+          )}
+        </>
+      )}
     </>
   );
 };
 
 describe('AuthProvider', () => {
-  let env: NodeJS.ProcessEnv;
+  describe('when Auth0 is configured', () => {
+    let env: NodeJS.ProcessEnv;
+    let getIdTokenClaims: jest.Mock;
+    let getTokenSilently: jest.Mock;
+    let isAuthenticated: boolean;
+    let isLoading: boolean;
+    let loginWithRedirect: jest.Mock;
+    let logout: jest.Mock;
+    let user: AuthUser;
 
-  beforeEach(() => {
-    env = {
-      ...process.env,
-    };
+    beforeEach(() => {
+      env = {
+        ...process.env,
+      };
 
-    process.env.REACT_APP_AUTH0_CLIENT_ID = 'AUTH0_CLIENT_ID';
-    process.env.REACT_APP_AUTH0_DOMAIN = 'AUTH0_DOMAIN';
-  });
+      process.env.REACT_APP_AUTH0_CLIENT_ID = 'AUTH0_CLIENT_ID';
+      process.env.REACT_APP_AUTH0_DOMAIN = 'AUTH0_DOMAIN';
 
-  afterEach(() => {
-    process.env = env;
-  });
+      getIdTokenClaims = jest.fn();
+      getIdTokenClaims = jest.fn();
+      loginWithRedirect = jest.fn();
+      logout = jest.fn();
+      user = {
+        name: 'Mo Gusbi',
+      };
+    });
 
-  it('should return values from Auth0', async () => {
-    const { findByTestId } = render(
-      <AuthProvider>
-        <AuthContext.Consumer>
-          {value => (
-            <>
-              <div data-testid="loaded">{!value?.isLoading && 'Loaded'}</div>
-              <div data-testid="authenticated">
-                {value?.isAuthenticated && 'Authenticated'}
-              </div>
-              <div data-testid="user">{value?.user?.name}</div>
-            </>
-          )}
-        </AuthContext.Consumer>
-      </AuthProvider>,
-    );
+    afterEach(() => {
+      process.env = env;
+    });
 
-    await expect(findByTestId('loaded')).resolves.toHaveTextContent('Loaded');
-    await expect(findByTestId('authenticated')).resolves.toHaveTextContent(
-      'Authenticated',
-    );
-    await expect(findByTestId('user')).resolves.toHaveTextContent('Mo Gusbi');
-  });
+    it('should show loading message when loading', async () => {
+      isLoading = true;
+      isAuthenticated = false;
 
-  it('should call Auth0', async () => {
-    const getIdTokenClaims = jest.fn();
-    const getTokenSilently = jest.fn();
-    const isAuthenticated = true;
-    const isLoading = false;
-    const loginWithRedirect = jest.fn();
-    const logout = jest.fn();
-    const user = jest.fn();
+      const { findByTestId } = render(
+        <AuthProvider>
+          <AuthContext.Provider
+            value={{
+              getIdTokenClaims,
+              getTokenSilently,
+              isAuthenticated,
+              isLoading,
+              loginWithRedirect,
+              logout,
+              user,
+            }}
+          >
+            <TestComponent />
+          </AuthContext.Provider>
+        </AuthProvider>,
+      );
 
-    const { findByTestId } = render(
-      <AuthProvider>
-        <AuthContext.Provider
-          value={{
-            getIdTokenClaims,
-            getTokenSilently,
-            isAuthenticated,
-            isLoading,
-            loginWithRedirect,
-            logout,
-            user,
-          }}
-        >
-          <TestComponent />
-        </AuthContext.Provider>
-      </AuthProvider>,
-    );
-    const logInButton = await findByTestId('log-in');
-    const logOutButton = await findByTestId('log-out');
+      await expect(findByTestId('loading')).resolves.toHaveTextContent(
+        'Loading...',
+      );
+    });
 
-    fireEvent.click(logInButton);
-    fireEvent.click(logOutButton);
+    it('should show user details and log out button when authorised', async () => {
+      isLoading = false;
+      isAuthenticated = true;
 
-    expect(loginWithRedirect).toHaveBeenCalled();
-    expect(logout).toHaveBeenCalledWith({
-      returnTo: 'somewhere',
+      const { findByTestId } = render(
+        <AuthProvider>
+          <AuthContext.Provider
+            value={{
+              getIdTokenClaims,
+              getTokenSilently,
+              isAuthenticated,
+              isLoading,
+              loginWithRedirect,
+              logout,
+              user,
+            }}
+          >
+            <TestComponent />
+          </AuthContext.Provider>
+        </AuthProvider>,
+      );
+      const logOutButton = await findByTestId('log-out');
+
+      fireEvent.click(logOutButton);
+
+      expect(logout).toHaveBeenCalledWith({
+        returnTo: 'somewhere',
+      });
+
+      await expect(findByTestId('user')).resolves.toHaveTextContent('Mo Gusbi');
+    });
+
+    it('should show log in button when not authorisd', async () => {
+      isLoading = false;
+      isAuthenticated = false;
+
+      const { findByTestId } = render(
+        <AuthProvider>
+          <AuthContext.Provider
+            value={{
+              getIdTokenClaims,
+              getTokenSilently,
+              isAuthenticated,
+              isLoading,
+              loginWithRedirect,
+              logout,
+              user,
+            }}
+          >
+            <TestComponent />
+          </AuthContext.Provider>
+        </AuthProvider>,
+      );
+      const logInButton = await findByTestId('log-in');
+
+      fireEvent.click(logInButton);
+
+      expect(loginWithRedirect).toHaveBeenCalled();
     });
   });
+
+  // it('should return values from Auth0', async () => {
+  //   const { findByTestId } = render(
+  //     <AuthProvider>
+  //       <AuthContext.Consumer>
+  //         {value => (
+  //           <>
+  //             <div data-testid="loaded">{!value?.isLoading && 'Loaded'}</div>
+  //             <div data-testid="authenticated">
+  //               {value?.isAuthenticated && 'Authenticated'}
+  //             </div>
+  //             <div data-testid="user">{value?.user?.name}</div>
+  //           </>
+  //         )}
+  //       </AuthContext.Consumer>
+  //     </AuthProvider>,
+  //   );
+
+  //   await expect(findByTestId('loaded')).resolves.toHaveTextContent('Loaded');
+  //   await expect(findByTestId('authenticated')).resolves.toHaveTextContent(
+  //     'Authenticated',
+  //   );
+  //   await expect(findByTestId('user')).resolves.toHaveTextContent('Mo Gusbi');
+  // });
+
+  // it('should call Auth0', async () => {
+  //   const getIdTokenClaims = jest.fn();
+  //   const getTokenSilently = jest.fn();
+  //   const isAuthenticated = true;
+  //   const isLoading = false;
+  //   const loginWithRedirect = jest.fn();
+  //   const logout = jest.fn();
+  //   const user = jest.fn();
+
+  //   const { findByTestId } = render(
+  //     <AuthProvider>
+  //       <AuthContext.Provider
+  //         value={{
+  //           getIdTokenClaims,
+  //           getTokenSilently,
+  //           isAuthenticated,
+  //           isLoading,
+  //           loginWithRedirect,
+  //           logout,
+  //           user,
+  //         }}
+  //       >
+  //         <TestComponent />
+  //       </AuthContext.Provider>
+  //     </AuthProvider>,
+  //   );
+  //   const logInButton = await findByTestId('log-in');
+  //   const logOutButton = await findByTestId('log-out');
+
+  //   fireEvent.click(logInButton);
+  //   fireEvent.click(logOutButton);
+
+  //   expect(loginWithRedirect).toHaveBeenCalled();
+  //   expect(logout).toHaveBeenCalledWith({
+  //     returnTo: 'somewhere',
+  //   });
+  // });
 });
