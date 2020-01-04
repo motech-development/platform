25cefca6fff34cfdc09ecc9d4e1071672d301db1
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-01-04 01:08:55 +0000

    chore(accounts): trying out lazy loading
    affects: @motech-development/accounts

--- applications/accounts/src/App.tsx
+++ applications/accounts/src/App.tsx
@@ -1,14 +1,18 @@
 import { ProtectedRoute, withAuth } from '@motech-development/auth';
-import React, { FC, memo } from 'react';
+import { Loader } from '@motech-development/breeze-ui';
+import React, { FC, lazy, memo, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import ProtectedPage from './containers/ProtectedPage';
-import PublicPage from './containers/PublicPage';
+
+const PublicPage = lazy(() => import('./containers/PublicPage'));
+const ProtectedPage = lazy(() => import('./containers/ProtectedPage'));
 
 const App: FC = () => (
-  <Switch>
-    <Route exact path="/" component={PublicPage} />
-    <ProtectedRoute exact path="/protected" component={ProtectedPage} />
-  </Switch>
+  <Suspense fallback={<Loader />}>
+    <Switch>
+      <Route exact path="/" component={PublicPage} />
+      <ProtectedRoute exact path="/protected" component={ProtectedPage} />
+    </Switch>
+  </Suspense>
 );
 
 export default memo(withAuth(App));
