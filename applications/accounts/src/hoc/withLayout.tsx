53dba7ef1e180f4894a47e6d37c70566ae802a3b
Mo Gusbi <mo@Mos-MacBook-Pro.local>
2020-01-06 19:49:26 +0000

    chore(accounts): appbar on the layout
    affects: @motech-development/accounts

--- applications/accounts/src/hoc/withLayout.tsx
+++ applications/accounts/src/hoc/withLayout.tsx
@@ -1,36 +1,29 @@
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useAuth } from '@motech-development/auth';
 import {
   AppBar,
   Button,
   Card,
+  Tooltip,
   Typography,
 } from '@motech-development/breeze-ui';
 import React, { ComponentType, memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
-const Avatar = styled.img`
-  border: 2px solid #fff;
-  border-radius: 50%;
-  display: inline-block;
-  height: 20px;
-  margin-left: 1rem;
-  vertical-align: middle;
-`;
-
 const AppName = styled(Typography)`
   && {
-    border-left: 5px solid #fff;
     flex-grow: 1;
-    margin: 0;
-    padding: 0 0 0 1rem;
+    margin: 0 0.5rem 0 0;
+    padding: 0;
   }
 `;
 
 const Username = styled(Typography)`
   && {
     flex-grow: 1;
-    margin: 0;
+    margin: 0 1rem 0 0.5rem;
     overflow: hidden;
     text-align: right;
     text-overflow: ellipsis;
@@ -50,22 +43,32 @@ const withLayout = (Component: ComponentType) =>
     return (
       <>
         <AppBar>
-          {/* TODO: Logo */}
           <AppName component="h1" variant="h4">
             {t('app-name')}
           </AppName>
 
           {user && (
-            <Username component="p" variant="p">
-              {user.name}
-            </Username>
+            <>
+              <Username component="p" variant="p">
+                {user.name}
+              </Username>
+
+              <Tooltip
+                id="sign-out"
+                parent={() => (
+                  <Button type="button" size="sm" onClick={logOut}>
+                    <FontAwesomeIcon icon={faSignOutAlt} />
+                  </Button>
+                )}
+                placement="bottom"
+                colour="primary"
+                message={t('log-out')}
+              />
+            </>
           )}
         </AppBar>
 
         <Card>
-          <Button type="button" size="sm" onClick={logOut}>
-            {user && <Avatar alt={user.name} src={user.picture} />}
-          </Button>
           <Component />
         </Card>
       </>
