439ccf88a7dc925b4292fa85cba5670828f1db4f
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-03-02 23:13:29 +0000

    chore(accounts): some page tests
    affects: @motech-development/accounts-client

---
+++ applications/accounts-client/src/pages/__tests__/NotFound.tsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import TestProvider from '../../utils/TestProvider';
+import NotFound from '../NotFound';
+
+describe('NotFound', () => {
+  it('should display the correct title', async () => {
+    const { findByText } = render(
+      <TestProvider>
+        <NotFound />
+      </TestProvider>,
+    );
+
+    await expect(findByText('not-found')).resolves.toBeInTheDocument();
+  });
+
+  it('should display the correct title', async () => {
+    const { findByText } = render(
+      <TestProvider>
+        <NotFound />
+      </TestProvider>,
+    );
+
+    await expect(findByText('description')).resolves.toBeInTheDocument();
+  });
+});
