29908b4a1e317cd9e9d0f37198152c8471420d44
Mo Gusbi <mo@mos-macbook-pro.home>
2020-03-11 11:08:15 +0000

    chore(ui): start toast tests
    affects: @motech-development/breeze-ui

--- packages/breeze-ui/src/setupTests.ts
+++ packages/breeze-ui/src/setupTests.ts
@@ -3,4 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import { randomFillSync } from 'crypto';
 import 'jest-styled-components';
+
+Object.defineProperty(window, 'crypto', {
+  value: {
+    getRandomValues: (buffer: NodeJS.ArrayBufferView) => randomFillSync(buffer),
+  },
+  writable: true,
+});
