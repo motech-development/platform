4f4ddaa970f7341cdfab04e5944bdbe1fe131965
Mo Gusbi <mo@mos-macbook-pro.home>
2020-03-20 13:02:26 +0000

    chore(accounts): more company delete stuff
    affects: @motech-development/accounts-api

---
+++ applications/accounts-api/src/handlers/delete-clients.ts
@@ -0,0 +1,67 @@
+// TODO: Publish to queue instead
+import { Handler } from 'aws-lambda';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+
+const { TABLE } = process.env;
+const documentClient = new DocumentClient();
+
+interface IEvent {
+  arguments: {
+    id: string;
+  };
+  field: string;
+  owner: string;
+}
+
+export const handler: Handler<IEvent> = async event => {
+  if (!TABLE) {
+    throw new Error('No table set');
+  }
+
+  const { field, owner } = event;
+
+  if (field !== 'deleteClients') {
+    throw new Error('Incorrect field');
+  }
+
+  const { id } = event.arguments;
+
+  const clients = await documentClient
+    .query({
+      ExpressionAttributeNames: {
+        '#data': 'data',
+        '#owner': 'owner',
+        '#typename': '__typename',
+      },
+      ExpressionAttributeValues: {
+        ':data': `${owner}:${id}`,
+        ':owner': owner,
+        ':typename': 'Client',
+      },
+      FilterExpression: '#owner = :owner',
+      IndexName: '__typename-data-index',
+      KeyConditionExpression:
+        '#typename = :typename AND begins_with(#data, :data)',
+      ProjectionExpression: 'id',
+      TableName: TABLE,
+    })
+    .promise();
+
+  // TODO: Chunk operation
+  await documentClient
+    .batchWrite({
+      RequestItems: {
+        [TABLE]: [
+          ...clients.Items?.map(item => ({
+            DeleteRequest: {
+              Key: {
+                __typename: 'Client',
+                id: item.id,
+              },
+            },
+          })),
+        ],
+      },
+    })
+    .promise();
+};
