acc6e239c429bb88d63788364884d49c92c6711a
Mo Gusbi <mo@mos-macbook-pro.home>
2020-03-26 18:17:28 +0000

    feat(ui): vertical align cols
    affects: @motech-development/breeze-ui

--- packages/breeze-ui/src/Col/Col.tsx
+++ packages/breeze-ui/src/Col/Col.tsx
@@ -2,20 +2,43 @@ import React, { FC, memo, ReactNode } from 'react';
 import styled from 'styled-components';
 
 interface IColContainer {
+  align: 'centre' | 'left' | 'right';
   lg: number;
   lgOffset: number;
   md: number;
   mdOffset: number;
   sm: number;
   smOffset: number;
+  verticalAlign: boolean;
   xs: number;
   xsOffset: number;
 }
 
 const ColContainer = styled.div<IColContainer>`
-  ${({ lg, lgOffset, md, mdOffset, sm, smOffset, xs, xsOffset }) => `
+  ${({
+    align,
+    lg,
+    lgOffset,
+    md,
+    mdOffset,
+    sm,
+    smOffset,
+    verticalAlign,
+    xs,
+    xsOffset,
+  }) => `
     grid-column: ${xsOffset > 0 ? `${xsOffset} / span ${xs}` : `span ${xs}`};
     position: relative;
+    text-align ${align === 'centre' ? 'center' : align};
+
+    ${
+      verticalAlign
+        ? `
+      align-items: center;
+      display: flex;
+    `
+        : ``
+    }
 
     ${
       sm
@@ -55,40 +78,54 @@ const ColContainer = styled.div<IColContainer>`
   `}
 `;
 
+const VericalAlignWrapper = styled.div`
+  flex: 1;
+`;
+
 export interface IColProps {
   children: ReactNode;
+  align?: 'centre' | 'left' | 'right';
   lg?: number;
   lgOffset?: number;
   md?: number;
   mdOffset?: number;
   sm?: number;
   smOffset?: number;
+  verticalAlign?: 'middle';
   xs?: number;
   xsOffset?: number;
 }
 
 const Col: FC<IColProps> = ({
   children,
+  align = 'left',
   lg = 0,
   lgOffset = 0,
   md = 0,
   mdOffset = 0,
   sm = 0,
   smOffset = 0,
+  verticalAlign = null,
   xs = 12,
   xsOffset = 0,
 }) => (
   <ColContainer
+    align={align}
     lg={lg}
     lgOffset={lgOffset}
     md={md}
     mdOffset={mdOffset}
     sm={sm}
     smOffset={smOffset}
+    verticalAlign={!!verticalAlign}
     xs={xs}
     xsOffset={xsOffset}
   >
-    {children}
+    {verticalAlign ? (
+      <VericalAlignWrapper>{children}</VericalAlignWrapper>
+    ) : (
+      children
+    )}
   </ColContainer>
 );
 
