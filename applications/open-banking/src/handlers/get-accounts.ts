6a992cfa6e538ddfd754276ccafd8e3cc22bec7c
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-03-29 12:40:05 +0100

    feat(banking): get accounts endpoint
    affects: @motech-development/open-banking

---
+++ applications/open-banking/src/handlers/get-accounts.ts
@@ -0,0 +1,67 @@
+import { APIGatewayEvent, Handler } from 'aws-lambda';
+import httpClient from '../shared/http-client';
+import proxyHandler from '../shared/proxy-handler';
+
+interface IAccounts {
+  data: {
+    accountIdentifications: {
+      type: string;
+      identification: string;
+    }[];
+    balance: string;
+    currency: string;
+    id: string;
+    type: string;
+  }[];
+}
+
+export const handler: Handler<APIGatewayEvent> = proxyHandler(async event => {
+  const { Consent } = event.headers;
+
+  if (!Consent) {
+    const response = {
+      body: JSON.stringify({
+        message: 'No consent',
+        statusCode: 400,
+      }),
+      statusCode: 400,
+    };
+
+    throw response;
+  }
+
+  const endpoint = '/accounts';
+
+  try {
+    const { data } = await httpClient.get<IAccounts>(endpoint, {
+      headers: {
+        Consent,
+      },
+    });
+
+    return {
+      body: JSON.stringify({
+        items: data.data.map(
+          ({ accountIdentifications, balance, currency, id, type }) => ({
+            accountIdentifications,
+            balance,
+            currency,
+            id,
+            type,
+          }),
+        ),
+      }),
+      statusCode: 200,
+    };
+  } catch (e) {
+    const { status } = e.response;
+
+    return {
+      body: JSON.stringify({
+        message: 'Unable to get accounts',
+        statusCode: status,
+      }),
+      statusCode: status,
+    };
+  }
+});
