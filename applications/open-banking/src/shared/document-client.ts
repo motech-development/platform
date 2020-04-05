4481f6e5bf187ea997ad44d71c11f06b56203711
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-04-05 14:26:55 +0100

    chore(banking): tweak tests
    affects: @motech-development/open-banking

--- applications/open-banking/src/shared/document-client.ts
+++ applications/open-banking/src/shared/document-client.ts
@@ -1,13 +1,14 @@
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
-const { STAGE = '' } = process.env;
+const documentClient = () => {
+  const { STAGE } = process.env;
 
-const documentClient =
-  STAGE === 'local'
+  return STAGE === 'local'
     ? new DocumentClient({
         endpoint: 'http://localhost:8000',
         region: 'localhost',
       })
     : new DocumentClient();
+};
 
-export default documentClient;
+export default documentClient();
