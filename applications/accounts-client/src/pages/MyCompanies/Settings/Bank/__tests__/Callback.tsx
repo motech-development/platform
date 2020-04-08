a14897c25a1bb9ee5852bf89dc9652472f4f9b0b
Mo Gusbi <mo@mos-macbook-pro.home>
2020-04-08 12:00:48 +0100

    chore(accounts): callback tests
    affects: @motech-development/accounts-client

---
+++ applications/accounts-client/src/pages/MyCompanies/Settings/Bank/__tests__/Callback.tsx
@@ -0,0 +1,146 @@
+import { MockedProvider, MockedResponse, wait } from '@apollo/react-testing';
+import { act, render, RenderResult } from '@testing-library/react';
+import { createMemoryHistory, MemoryHistory } from 'history';
+import React from 'react';
+import UPDATE_BANK_SETTINGS from '../../../../../graphql/bank/UPDATE_BANK_SETTINGS';
+import TestProvider from '../../../../../utils/TestProvider';
+import Callback from '../Callback';
+
+describe('Callback', () => {
+  let component: RenderResult;
+  let history: MemoryHistory;
+  let mocks: MockedResponse[];
+
+  describe('with a successful response', () => {
+    beforeEach(() => {
+      history = createMemoryHistory({
+        initialEntries: [
+          '/settings/company-id/bank/callback?institution=bank-name&consent=consent-token&user-uuid=user-id',
+        ],
+      });
+
+      jest.spyOn(history, 'push');
+
+      mocks = [
+        {
+          request: {
+            query: UPDATE_BANK_SETTINGS,
+            variables: {
+              input: {
+                bank: 'bank-name',
+                consent: 'consent-token',
+                id: 'company-id',
+                user: 'user-id',
+              },
+            },
+          },
+          result: {
+            data: {
+              updateBankSettings: {
+                account: null,
+                id: 'company-id',
+                user: 'user-id',
+              },
+            },
+          },
+        },
+      ];
+
+      component = render(
+        <TestProvider
+          path="/settings/:companyId/bank/callback"
+          history={history}
+        >
+          <MockedProvider mocks={mocks} addTypename={false}>
+            <Callback />
+          </MockedProvider>
+        </TestProvider>,
+      );
+    });
+
+    it('should redirect to the account selection', async () => {
+      const { findByTestId } = component;
+
+      await act(async () => {
+        await wait(0);
+
+        await findByTestId('next-page');
+      });
+
+      expect(history.push).toHaveBeenCalledWith(
+        '/my-companies/settings/company-id/bank/select-account',
+      );
+    });
+  });
+
+  describe('with an unsuccessful response', () => {
+    beforeEach(() => {
+      history = createMemoryHistory({
+        initialEntries: [
+          '/settings/company-id/bank/callback?institution=bank-name&consent=consent-token',
+        ],
+      });
+
+      jest.spyOn(history, 'push');
+
+      mocks = [
+        {
+          request: {
+            query: UPDATE_BANK_SETTINGS,
+            variables: {
+              input: {
+                bank: 'bank-name',
+                consent: 'consent-token',
+                id: 'company-id',
+                user: null,
+              },
+            },
+          },
+          result: {
+            data: {
+              updateBankSettings: {
+                account: null,
+                id: 'company-id',
+                user: null,
+              },
+            },
+          },
+        },
+      ];
+
+      component = render(
+        <TestProvider
+          path="/settings/:companyId/bank/callback"
+          history={history}
+        >
+          <MockedProvider mocks={mocks} addTypename={false}>
+            <Callback />
+          </MockedProvider>
+        </TestProvider>,
+      );
+    });
+
+    it('should show the error card', async () => {
+      const { findByText } = component;
+
+      await act(async () => {
+        await wait(0);
+      });
+
+      await expect(findByText('callback.error')).resolves.toBeInTheDocument();
+    });
+
+    it('should have the correct back link', async () => {
+      const { findByText } = component;
+
+      await act(async () => {
+        await wait(0);
+      });
+
+      await expect(findByText('go-back')).resolves.toHaveAttribute(
+        'href',
+        '/my-companies/settings/company-id',
+      );
+    });
+  });
+});
