0a767728d27c0496cb0c17a10a8c81687e8775b0
Mo Gusbi <mo@mos-macbook-pro.home>
2020-04-28 17:37:24 +0100

    chore(accounts): get balance tests
    affects: @motech-development/accounts-api

--- applications/accounts-api/src/handlers/get-balance.ts
+++ applications/accounts-api/src/handlers/get-balance.ts
@@ -20,7 +20,7 @@ const getBalance = async (event: IEvent) => {
   }
 
   // TODO: Find a better solution than having an artificial delay
-  await delay(1000);
+  await delay(500);
 
   const { id, owner } = event;
 
