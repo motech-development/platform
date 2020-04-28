ca3749702a4c6e6bc0b225a899d4b7888a1a5298
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-04-28 23:00:25 +0100

    chore(accounts): start data tests
    affects: @motech-development/accounts-data

--- applications/accounts-data/src/shared/__tests__/aggregated-day.ts
+++ applications/accounts-data/src/shared/__tests__/aggregated-day.ts
@@ -1,3 +1,9 @@
+import aggregatedDay from '../aggregated-day';
+
 describe('aggregated-day', () => {
-  it.todo('should return the correct date');
+  it('should return the correct date', () => {
+    expect(aggregatedDay('2020-04-28T20:29:21Z')).toEqual(
+      '2020-04-28T00:00:00.000Z',
+    );
+  });
 });
