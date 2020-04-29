5cbec457890af2ffef617448fae8320b63d90061
Mo Gusbi <mo@mos-macbook-pro.home>
2020-04-29 17:11:51 +0100

    chore(accounts): balance tests
    affects: @motech-development/accounts-data

--- applications/accounts-data/src/__tests__/balance.ts
+++ applications/accounts-data/src/__tests__/balance.ts
@@ -1,11 +1,366 @@
+import { Context } from 'aws-lambda';
+import ctx from 'aws-lambda-mock-context';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { handler } from '../balance';
+
 describe('balance', () => {
-  describe('with no table set', () => {
-    it.todo('should throw an error');
+  let callback: jest.Mock;
+  let context: Context;
+
+  beforeEach(() => {
+    context = ctx();
+
+    context.done();
+
+    callback = jest.fn();
+  });
+
+  it('should throw an error if no table is set', async () => {
+    const event = {
+      Records: [],
+    };
+
+    await expect(handler(event, context, callback)).rejects.toThrow(
+      'No table set',
+    );
   });
 
   describe('with table set', () => {
-    it.todo('should do nothing if there is nothing to process');
+    let env: NodeJS.ProcessEnv;
 
-    it.todo('should begin transaction if there are things to process');
+    beforeEach(() => {
+      env = {
+        ...process.env,
+      };
+
+      process.env.TABLE = 'app-table';
+    });
+
+    afterEach(() => {
+      process.env = env;
+    });
+
+    describe('when there are no errors thrown by DynamoDB', () => {
+      beforeEach(() => {
+        DocumentClient.prototype.update = jest.fn().mockReturnValue({
+          promise: jest.fn(),
+        });
+      });
+
+      it('should do nothing if there is nothing to process', async () => {
+        const event = {
+          Records: [],
+        };
+
+        await handler(event, context, callback);
+
+        expect(DocumentClient.prototype.update).toHaveBeenCalledTimes(0);
+      });
+
+      it('should update the correct number of records', async () => {
+        const event = {
+          Records: [
+            {
+              dynamodb: {
+                NewImage: {
+                  __typename: {
+                    S: 'Transaction',
+                  },
+                  amount: {
+                    N: '100.25',
+                  },
+                  category: {
+                    S: 'Sales',
+                  },
+                  companyId: {
+                    S: 'company-id',
+                  },
+                  date: {
+                    S: '2019-12-15T00:00:00.000Z',
+                  },
+                  vat: {
+                    N: '1.2',
+                  },
+                },
+                OldImage: {
+                  __typename: {
+                    S: 'Transaction',
+                  },
+                  amount: {
+                    N: '200.5',
+                  },
+                  category: {
+                    S: 'Sales',
+                  },
+                  companyId: {
+                    S: 'company-id',
+                  },
+                  date: {
+                    S: '2019-12-15T00:00:00.000Z',
+                  },
+                  vat: {
+                    N: '2.4',
+                  },
+                },
+              },
+              eventName: 'INSERT' as const,
+            },
+            {
+              dynamodb: {
+                NewImage: {
+                  __typename: {
+                    S: 'Transaction',
+                  },
+                  amount: {
+                    N: '100.25',
+                  },
+                  category: {
+                    S: 'Expenses',
+                  },
+                  companyId: {
+                    S: 'company-id',
+                  },
+                  date: {
+                    S: '2019-12-15T00:00:00.000Z',
+                  },
+                  vat: {
+                    N: '1.2',
+                  },
+                },
+                OldImage: {
+                  __typename: {
+                    S: 'Transaction',
+                  },
+                  amount: {
+                    N: '200.5',
+                  },
+                  category: {
+                    S: 'Expenses',
+                  },
+                  companyId: {
+                    S: 'company-id',
+                  },
+                  date: {
+                    S: '2019-12-15T00:00:00.000Z',
+                  },
+                  vat: {
+                    N: '2.4',
+                  },
+                },
+              },
+              eventName: 'MODIFY' as const,
+            },
+            {
+              dynamodb: {
+                NewImage: {
+                  __typename: {
+                    S: 'Transaction',
+                  },
+                  amount: {
+                    N: '100.25',
+                  },
+                  category: {
+                    S: 'Sales',
+                  },
+                  companyId: {
+                    S: 'company-id',
+                  },
+                  date: {
+                    S: '2019-12-15T00:00:00.000Z',
+                  },
+                  vat: {
+                    N: '1.2',
+                  },
+                },
+                OldImage: {
+                  __typename: {
+                    S: 'Transaction',
+                  },
+                  amount: {
+                    N: '200.5',
+                  },
+                  category: {
+                    S: 'Sales',
+                  },
+                  companyId: {
+                    S: 'company-id',
+                  },
+                  date: {
+                    S: '2019-12-15T00:00:00.000Z',
+                  },
+                  vat: {
+                    N: '2.4',
+                  },
+                },
+              },
+              eventName: 'REMOVE' as const,
+            },
+          ],
+        };
+
+        await handler(event, context, callback);
+
+        expect(DocumentClient.prototype.update).toHaveBeenCalledTimes(3);
+      });
+    });
+
+    describe('when DyanmoDB throws an error', () => {
+      beforeEach(() => {
+        DocumentClient.prototype.update = jest.fn().mockReturnValue({
+          promise: jest
+            .fn()
+            .mockRejectedValue(new Error('Something has gone wrong')),
+        });
+
+        jest.spyOn(console, 'error').mockReturnValue();
+      });
+
+      it('should swallow the error', async () => {
+        const event = {
+          Records: [
+            {
+              dynamodb: {
+                NewImage: {
+                  __typename: {
+                    S: 'Transaction',
+                  },
+                  amount: {
+                    N: '100.25',
+                  },
+                  category: {
+                    S: 'Sales',
+                  },
+                  companyId: {
+                    S: 'company-id',
+                  },
+                  date: {
+                    S: '2019-12-15T00:00:00.000Z',
+                  },
+                  vat: {
+                    N: '1.2',
+                  },
+                },
+                OldImage: {
+                  __typename: {
+                    S: 'Transaction',
+                  },
+                  amount: {
+                    N: '200.5',
+                  },
+                  category: {
+                    S: 'Sales',
+                  },
+                  companyId: {
+                    S: 'company-id',
+                  },
+                  date: {
+                    S: '2019-12-15T00:00:00.000Z',
+                  },
+                  vat: {
+                    N: '2.4',
+                  },
+                },
+              },
+              eventName: 'INSERT' as const,
+            },
+            {
+              dynamodb: {
+                NewImage: {
+                  __typename: {
+                    S: 'Transaction',
+                  },
+                  amount: {
+                    N: '100.25',
+                  },
+                  category: {
+                    S: 'Expenses',
+                  },
+                  companyId: {
+                    S: 'company-id',
+                  },
+                  date: {
+                    S: '2019-12-15T00:00:00.000Z',
+                  },
+                  vat: {
+                    N: '1.2',
+                  },
+                },
+                OldImage: {
+                  __typename: {
+                    S: 'Transaction',
+                  },
+                  amount: {
+                    N: '200.5',
+                  },
+                  category: {
+                    S: 'Expenses',
+                  },
+                  companyId: {
+                    S: 'company-id',
+                  },
+                  date: {
+                    S: '2019-12-15T00:00:00.000Z',
+                  },
+                  vat: {
+                    N: '2.4',
+                  },
+                },
+              },
+              eventName: 'MODIFY' as const,
+            },
+            {
+              dynamodb: {
+                NewImage: {
+                  __typename: {
+                    S: 'Transaction',
+                  },
+                  amount: {
+                    N: '100.25',
+                  },
+                  category: {
+                    S: 'Sales',
+                  },
+                  companyId: {
+                    S: 'company-id',
+                  },
+                  date: {
+                    S: '2019-12-15T00:00:00.000Z',
+                  },
+                  vat: {
+                    N: '1.2',
+                  },
+                },
+                OldImage: {
+                  __typename: {
+                    S: 'Transaction',
+                  },
+                  amount: {
+                    N: '200.5',
+                  },
+                  category: {
+                    S: 'Sales',
+                  },
+                  companyId: {
+                    S: 'company-id',
+                  },
+                  date: {
+                    S: '2019-12-15T00:00:00.000Z',
+                  },
+                  vat: {
+                    N: '2.4',
+                  },
+                },
+              },
+              eventName: 'REMOVE' as const,
+            },
+          ],
+        };
+
+        await handler(event, context, callback);
+
+        // eslint-disable-next-line no-console
+        expect(console.error).toHaveBeenCalledWith('Something has gone wrong');
+      });
+    });
   });
 });
