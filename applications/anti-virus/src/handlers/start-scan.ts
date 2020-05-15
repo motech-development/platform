82e2d6e06756f5cc1acdbb9feb44930e5df8eab2
Mo Gusbi <mo@mos-macbook-pro.home>
2020-05-15 18:31:43 +0100

    chore(av): end to end
    affects: @motech-development/accounts-storage, @motech-development/anti-virus,
    @motech-development/open-banking

---
+++ applications/anti-virus/src/handlers/start-scan.ts
@@ -0,0 +1,30 @@
+import { SQSHandler } from 'aws-lambda';
+import { StepFunctions } from 'aws-sdk';
+
+const stepFunctions = new StepFunctions();
+
+export const handler: SQSHandler = async event => {
+  const { STATE_MACHINE_ARN } = process.env;
+
+  if (!STATE_MACHINE_ARN) {
+    throw new Error('No state machine set');
+  }
+
+  const executions = event.Records.map(record => {
+    const { messageAttributes } = record;
+    const { from, key, to } = messageAttributes;
+
+    return stepFunctions
+      .startExecution({
+        input: JSON.stringify({
+          from: from.stringValue,
+          key: key.stringValue,
+          to: to.stringValue,
+        }),
+        stateMachineArn: STATE_MACHINE_ARN,
+      })
+      .promise();
+  });
+
+  await Promise.all(executions);
+};
