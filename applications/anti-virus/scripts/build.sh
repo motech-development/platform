22da8ac27d22a57a4df12fa407e663a2bb269f8e
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-05-15 23:54:04 +0100

    chore(av): begin av ci deploy
    affects: @motech-development/anti-virus

--- applications/anti-virus/scripts/build.sh
+++ applications/anti-virus/scripts/build.sh
@@ -12,13 +12,13 @@ docker create -i -t -v ${PWD}/clamav:/home/docker  --name s3-antivirus-builder a
 docker start s3-antivirus-builder
 
 echo "-- Updating, downloading and unpacking clamAV and ClamAV update --"
-docker exec -it -w /home/docker s3-antivirus-builder yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-docker exec -it -w /home/docker s3-antivirus-builder yum -y install clamav clamav-lib clamav-update json-c pcre2 libxml2 bzip2-libs libtool-ltdl xz-libs
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "echo 'folder content' && ls -la"
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "ldd /usr/bin/clamscan | grep '=>' | grep -v -e '^$' | awk '{print \$3}' | xargs -I % sh -c 'cp %* .'"
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "ldd /usr/bin/freshclam | grep '=>' | grep -v -e '^$' | awk '{print \$3}' | xargs -I % sh -c 'cp %* .'"
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "cp /usr/bin/clamscan ."
-docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "cp /usr/bin/freshclam ."
+docker exec -t -w /home/docker s3-antivirus-builder yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+docker exec -t -w /home/docker s3-antivirus-builder yum -y install clamav clamav-lib clamav-update json-c pcre2 libxml2 bzip2-libs libtool-ltdl xz-libs
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "echo 'folder content' && ls -la"
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "ldd /usr/bin/clamscan | grep '=>' | grep -v -e '^$' | awk '{print \$3}' | xargs -I % sh -c 'cp %* .'"
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "ldd /usr/bin/freshclam | grep '=>' | grep -v -e '^$' | awk '{print \$3}' | xargs -I % sh -c 'cp %* .'"
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "cp /usr/bin/clamscan ."
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "cp /usr/bin/freshclam ."
 
 docker stop s3-antivirus-builder
 docker rm s3-antivirus-builder
