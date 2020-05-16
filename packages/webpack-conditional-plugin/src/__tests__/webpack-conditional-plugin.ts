a6f6a7d8638b8be5983ea74eb38a7663ed7bfc95
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-05-16 14:30:35 +0100

    chore(webpack): conditional plugin tests
    affects: @motech-development/webpack-conditional-plugin

--- packages/webpack-conditional-plugin/src/__tests__/webpack-conditional-plugin.ts
+++ packages/webpack-conditional-plugin/src/__tests__/webpack-conditional-plugin.ts
@@ -1,3 +1,36 @@
+import { Compiler } from 'webpack';
+import ConditionalPlugin from '../webpack-conditional-plugin';
+
+class MockPlugin {
+  public apply: jest.Mock;
+
+  constructor(apply: jest.Mock) {
+    this.apply = apply;
+  }
+}
+
 describe('webpack-conditional-plugin', () => {
-  it.todo('should be tested');
+  let apply: jest.Mock;
+  let compiler: Compiler;
+
+  beforeEach(() => {
+    apply = jest.fn();
+    compiler = {} as Compiler;
+  });
+
+  it('should trigger the plugin if the condition is met', () => {
+    const plugin = new ConditionalPlugin(() => true, new MockPlugin(apply));
+
+    plugin.apply(compiler);
+
+    expect(apply).toHaveBeenCalled();
+  });
+
+  it('should not trigger the plugin if the condition is not met', () => {
+    const plugin = new ConditionalPlugin(() => false, new MockPlugin(apply));
+
+    plugin.apply(compiler);
+
+    expect(apply).not.toHaveBeenCalled();
+  });
 });
