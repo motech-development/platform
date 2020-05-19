7586168ffb697bdc67c121ea2aa81bf753c908c9
Mo Gusbi <mo@mos-macbook-pro.home>
2020-05-19 17:37:28 +0100

    chore(accounts): delete file queue
    affects: @motech-development/accounts-storage

---
+++ applications/accounts-storage/src/handlers/process-delete.ts
@@ -0,0 +1,30 @@
+import { SQSHandler } from 'aws-lambda';
+import { S3 } from 'aws-sdk';
+
+const s3 = new S3();
+
+export const handler: SQSHandler = async event => {
+  const { DOWNLOAD_BUCKET } = process.env;
+
+  if (!DOWNLOAD_BUCKET) {
+    throw new Error('No destination bucket set');
+  }
+
+  const deletions = event.Records.map(record => {
+    const { messageAttributes } = record;
+    const { key } = messageAttributes;
+
+    if (key.stringValue) {
+      return s3
+        .deleteObject({
+          Bucket: DOWNLOAD_BUCKET,
+          Key: key.stringValue,
+        })
+        .promise();
+    }
+
+    return null;
+  });
+
+  await Promise.all(deletions);
+};
