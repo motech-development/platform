b27cbb04fb29fc5421428877ebe482d99679b9fd
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-05-24 23:56:51 +0100

    chore(accounts): lambda@edge handler tests
    affects: @motech-development/accounts-infrastructure

--- applications/accounts-infrastructure/src/handlers/__tests__/cache.ts
+++ applications/accounts-infrastructure/src/handlers/__tests__/cache.ts
@@ -1,7 +1,107 @@
+import { CloudFrontResponseEvent, Context } from 'aws-lambda';
+import ctx from 'aws-lambda-mock-context';
+import { handler } from '../cache';
+
 describe('cache', () => {
-  it.todo('should not cache index.html');
+  let callback: jest.Mock;
+  let context: Context;
+  let event: CloudFrontResponseEvent;
 
-  it.todo('should not cache i18n assets');
+  beforeEach(() => {
+    context = ctx();
 
-  it.todo('should cache all other assets');
+    context.done();
+
+    callback = jest.fn();
+
+    event = {
+      Records: [
+        {
+          cf: {
+            request: {},
+            response: {
+              headers: {},
+              status: '200',
+            },
+          },
+        },
+      ],
+    } as CloudFrontResponseEvent;
+  });
+
+  it('should do nothing if the status code is not 200', () => {
+    const { response } = event.Records[0].cf;
+
+    response.status = '400';
+
+    handler(event, context, callback);
+
+    expect(callback).toHaveBeenCalledWith(null, response);
+  });
+
+  it('should not cache index.html', () => {
+    const { request, response } = event.Records[0].cf;
+
+    request.uri = 'index.html';
+
+    handler(event, context, callback);
+
+    const result = {
+      ...response,
+      headers: {
+        'Cache-Control': [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate',
+          },
+        ],
+      },
+    };
+
+    expect(callback).toHaveBeenCalledWith(null, result);
+  });
+
+  it('should not cache i18n assets', () => {
+    const { request, response } = event.Records[0].cf;
+
+    request.uri = 'locales/en/global.json';
+
+    handler(event, context, callback);
+
+    const result = {
+      ...response,
+      headers: {
+        'Cache-Control': [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate',
+          },
+        ],
+      },
+    };
+
+    expect(callback).toHaveBeenCalledWith(null, result);
+  });
+
+  it('should cache all other assets', () => {
+    const { request, response } = event.Records[0].cf;
+
+    request.uri = 'favicon.ico';
+
+    handler(event, context, callback);
+
+    const result = {
+      ...response,
+      headers: {
+        'Cache-Control': [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000',
+          },
+        ],
+      },
+    };
+
+    expect(callback).toHaveBeenCalledWith(null, result);
+  });
 });
--- applications/accounts-infrastructure/src/handlers/cache.ts
+++ applications/accounts-infrastructure/src/handlers/cache.ts
@@ -2,25 +2,27 @@ import { CloudFrontResponseHandler } from 'aws-lambda';
 
 export const handler: CloudFrontResponseHandler = (event, _, callback) => {
   const { request, response } = event.Records[0].cf;
-  const { headers } = response;
+  const { headers, status } = response;
 
-  switch (true) {
-    case request.uri.indexOf('index.html') !== -1:
-    case request.uri.indexOf('locales') !== -1:
-      headers['Cache-Control'] = [
-        {
-          key: 'Cache-Control',
-          value: 'no-store, no-cache, must-revalidate',
-        },
-      ];
-      break;
-    default:
-      headers['Cache-Control'] = [
-        {
-          key: 'Cache-Control',
-          value: 'public, max-age=31536000',
-        },
-      ];
+  if (status === '200') {
+    switch (true) {
+      case request.uri.indexOf('index.html') !== -1:
+      case request.uri.indexOf('locales') !== -1:
+        headers['Cache-Control'] = [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate',
+          },
+        ];
+        break;
+      default:
+        headers['Cache-Control'] = [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000',
+          },
+        ];
+    }
   }
 
   callback(null, response);
