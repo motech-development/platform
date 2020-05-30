e49bdf09c162ad2a5ed08317f6401e41130833da
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-05-30 13:21:27 +0100

    chore(accounts): balance fixes
    affects: @motech-development/accounts-api, @motech-development/accounts-client

--- applications/accounts-api/src/shared/__tests__/transform-balance.ts
+++ applications/accounts-api/src/shared/__tests__/transform-balance.ts
@@ -13,6 +13,7 @@ describe('transform-balance', () => {
       currency: 'GBP',
       id: 'company-id',
       items: {},
+      openingBalance: 0,
       owner: 'owner',
       vat: {
         owed: 0,
@@ -31,6 +32,7 @@ describe('transform-balance', () => {
       currency: 'GBP',
       id: 'company-id',
       items: {},
+      openingBalance: 0,
       owner: 'owner',
       vat: {
         owed: 0,
@@ -63,6 +65,7 @@ describe('transform-balance', () => {
         '2019-12-15T00:00:00.000Z': -349,
         '2019-12-31T00:00:00.000Z': 2960.91,
       },
+      openingBalance: 0,
       owner: 'owner',
       vat: {
         owed: 493.48,
@@ -178,6 +181,157 @@ describe('transform-balance', () => {
         },
         {
           balance: -190,
+          currency: 'GBP',
+          date: '2019-11-25T00:00:00.000Z',
+          items: [
+            {
+              amount: -190,
+              category: 'Accommodation',
+              companyId: 'company-id',
+              date: '2019-11-25T00:00:00.000Z',
+              description: 'Room',
+              id: 'transaction-2',
+              name: 'Hotel',
+              status: 'confirmed',
+              vat: 31.67,
+            },
+          ],
+        },
+      ],
+      vat: {
+        owed: 493.48,
+        paid: 98.54,
+      },
+    });
+  });
+
+  it('should return balance when there are transactions and starting balance', () => {
+    const balance = {
+      balance: 2519.71,
+      currency: 'GBP',
+      id: 'company-id',
+      items: {
+        '2019-11-25T00:00:00.000Z': -190,
+        '2019-12-04T00:00:00.000Z': -2.2,
+        '2019-12-15T00:00:00.000Z': -349,
+        '2019-12-31T00:00:00.000Z': 2960.91,
+      },
+      openingBalance: 100,
+      owner: 'owner',
+      vat: {
+        owed: 493.48,
+        paid: 98.54,
+      },
+    };
+    const transactions = [
+      {
+        amount: -2.2,
+        category: '',
+        companyId: 'company-id',
+        date: '2019-12-04T00:00:00.000Z',
+        description: 'Food',
+        id: 'transaction-1',
+        name: 'Canteen',
+        status: 'confirmed',
+        vat: 0.37,
+      },
+      {
+        amount: -190,
+        category: 'Accommodation',
+        companyId: 'company-id',
+        date: '2019-11-25T00:00:00.000Z',
+        description: 'Room',
+        id: 'transaction-2',
+        name: 'Hotel',
+        status: 'confirmed',
+        vat: 31.67,
+      },
+      {
+        amount: -349,
+        category: 'Equipment',
+        companyId: 'company-id',
+        date: '2019-12-15T00:00:00.000Z',
+        description: 'iPad',
+        id: 'transaction-3',
+        name: 'Apple',
+        status: 'confirmed',
+        vat: 66.5,
+      },
+      {
+        amount: 2960.91,
+        category: 'Sales',
+        companyId: 'company-id',
+        date: '2019-12-31T00:00:00.000Z',
+        description: 'Invoice #1',
+        id: 'transaction-1',
+        name: 'Client',
+        status: 'confirmed',
+        vat: 493.48,
+      },
+    ];
+
+    expect(transformBalance(balance, transactions)).toEqual({
+      balance: 2519.71,
+      currency: 'GBP',
+      id: 'company-id',
+      owner: 'owner',
+      transactions: [
+        {
+          balance: 2519.71,
+          currency: 'GBP',
+          date: '2019-12-31T00:00:00.000Z',
+          items: [
+            {
+              amount: 2960.91,
+              category: 'Sales',
+              companyId: 'company-id',
+              date: '2019-12-31T00:00:00.000Z',
+              description: 'Invoice #1',
+              id: 'transaction-1',
+              name: 'Client',
+              status: 'confirmed',
+              vat: 493.48,
+            },
+          ],
+        },
+        {
+          balance: -441.2,
+          currency: 'GBP',
+          date: '2019-12-15T00:00:00.000Z',
+          items: [
+            {
+              amount: -349,
+              category: 'Equipment',
+              companyId: 'company-id',
+              date: '2019-12-15T00:00:00.000Z',
+              description: 'iPad',
+              id: 'transaction-3',
+              name: 'Apple',
+              status: 'confirmed',
+              vat: 66.5,
+            },
+          ],
+        },
+        {
+          balance: -92.2,
+          currency: 'GBP',
+          date: '2019-12-04T00:00:00.000Z',
+          items: [
+            {
+              amount: -2.2,
+              category: '',
+              companyId: 'company-id',
+              date: '2019-12-04T00:00:00.000Z',
+              description: 'Food',
+              id: 'transaction-1',
+              name: 'Canteen',
+              status: 'confirmed',
+              vat: 0.37,
+            },
+          ],
+        },
+        {
+          balance: -90,
           currency: 'GBP',
           date: '2019-11-25T00:00:00.000Z',
           items: [
