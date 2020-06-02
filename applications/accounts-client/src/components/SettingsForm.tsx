8263a5a971e3b636485a3a85b0f0d74f83a5e0db
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-06-02 17:27:27 +0100

    chore(accounts): remove useless lead text
    affects: @motech-development/accounts-client

--- applications/accounts-client/src/components/SettingsForm.tsx
+++ applications/accounts-client/src/components/SettingsForm.tsx
@@ -83,10 +83,6 @@ const SettingsForm: FC<ISettingsFormProps> = ({
                   {t('settings-form.expense-categories.title')}
                 </Typography>
 
-                <Typography component="p" variant="lead">
-                  {t('settings-form.expense-categories.lead')}
-                </Typography>
-
                 <FieldArray
                   name="categories"
                   render={arrayHelpers => (
