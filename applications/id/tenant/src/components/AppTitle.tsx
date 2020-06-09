94ad10212edb4abaa3117af770d5a835e1ee483f
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-06-09 20:21:10 +0100

    chore(id): conditional app title
    affects: @id/tenant

--- applications/id/tenant/src/components/AppTitle.tsx
+++ applications/id/tenant/src/components/AppTitle.tsx
@@ -2,13 +2,17 @@ import { Typography } from '@motech-development/breeze-ui';
 import React, { FC, memo } from 'react';
 
 const AppTitle: FC = () => {
-  const { title } = window.config.dict.signin;
+  if (window.config) {
+    const { title } = window.config.dict.signin;
 
-  return (
-    <Typography align="center" component="h2" variant="h1" margin="lg">
-      {title}
-    </Typography>
-  );
+    return (
+      <Typography align="center" component="h2" variant="h1" margin="lg">
+        {title}
+      </Typography>
+    );
+  }
+
+  return null;
 };
 
 export default memo(AppTitle);
