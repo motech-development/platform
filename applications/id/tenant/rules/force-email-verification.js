be3fd656f6b2b80309c5743b421ee45ed63a3357
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-06-21 16:37:43 +0100

    chore(id): fix up verification rule
    affects: @id/tenant

--- applications/id/tenant/rules/force-email-verification.js
+++ applications/id/tenant/rules/force-email-verification.js
@@ -1,22 +1,23 @@
 async function emailVerified(user, context, callback) {
+  const { ManagementClient } = require('auth0@2.23.0');
+
+  const managementClient = new ManagementClient({
+    domain: auth0.domain,
+    token: auth0.accessToken,
+  });
+
+  const metadata = user.user_metadata || {};
+
   const resendVerificationEmailIfExpired = async () => {
     const expirationPeriod = 1000 * 60 * 60 * 24;
-    const lastSent = user.user_metadata.emailVerificationSentDate;
+    const lastSent = metadata.emailVerificationSentDate;
     const now = new Date().valueOf();
 
     if (lastSent && now - lastSent < expirationPeriod) {
       return;
     }
 
-    const { ManagementClient } = require('auth0@2.23.0');
-    const managementClient = new ManagementClient({
-      domain: auth0.domain,
-      token: auth0.accessToken,
-    });
-    const metadata = {
-      ...user.user_metadata,
-      emailVerificationSentDate: now,
-    };
+    metadata.emailVerificationSentDate = now;
 
     try {
       await Promise.all([
@@ -35,14 +36,27 @@ async function emailVerified(user, context, callback) {
     }
   };
 
-  user.user_metadata = user.user_metadata || {};
-
   if (!user.email_verified) {
     await resendVerificationEmailIfExpired();
 
     return callback(
       new UnauthorizedError('Please verify your email before logging in.'),
     );
+  }
+
+  if (metadata.emailVerificationSentDate) {
+    delete metadata.emailVerificationSentDate;
+
+    try {
+      await managementClient.updateUserMetadata(
+        {
+          id: user.user_id,
+        },
+        metadata,
+      );
+    } catch (e) {
+      console.log(e.message);
+    }
   }
 
   return callback(null, user, context);
