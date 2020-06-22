7de6d69416ee9551b8066b41bf8d05d9eb7c40d0
Mo Gusbi <mo@mos-macbook-pro.home>
2020-06-22 11:46:17 +0100

    chore(id): rule test test
    affects: @id/tenant

--- applications/id/tenant/src/rules/__tests__/force-email-verification.js
+++ applications/id/tenant/src/rules/__tests__/force-email-verification.js
@@ -1,16 +1,43 @@
+const loadRule = require('../../utils/loadRule');
+
+const load = loadRule('../rules/force-email-verification.js', {
+  accessToken: 'access-token',
+  domain: 'https://test.com',
+});
+
 describe('force-email-verification', () => {
+  let rule;
+  let callback;
+  let context;
+  let user;
+
+  beforeEach(() => {
+    rule = load('emailVerified');
+    callback = jest.fn();
+    context = {};
+    user = {};
+  });
+
   describe('when user is verified', () => {
+    beforeEach(() => {
+      user.email_verified = true;
+    });
+
     it.todo('should remove emailVerificationSentDate if it exists');
 
     it.todo(
       'should not update meta data if emailVerificationSentDate does not exist',
     );
 
-    it.todo('should call the callback with the correct params');
+    it('should call the callback with the correct params', () => {
+      rule(user, context, callback);
+
+      expect(callback).toHaveBeenCalledWith(null, user, context);
+    });
   });
 
   describe('when user is not verified', () => {
-    it.todo('should call the callback with the correct params');
+    it.todo('should throw an UnauthorizedError');
 
     it.todo(
       'should not resend the verification email if it was already sent today',
---
+++ applications/id/tenant/src/utils/loadRule.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-underscore-dangle */
+const rewire = require('rewire');
+
+const requireWithVersionSupport = moduleName => {
+  const name = moduleName.split('@')[0];
+
+  // eslint-disable-next-line import/no-dynamic-require
+  return require(name);
+};
+
+const loadRule = (location, auth0 = {}) => {
+  const module = rewire(location);
+
+  module.__set__({
+    auth0,
+    require: requireWithVersionSupport,
+  });
+
+  return module.__get__;
+};
+
+module.exports = loadRule;
