2ce1dd10c9f3b71b7d31251384cc6d1106224cb0
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-07-01 19:44:16 +0100

    chore(accounts): add typeahead suggestions from pending
    affects: @accounts/data

--- applications/accounts/data/src/handlers/__tests__/insert-typeahead.ts
+++ applications/accounts/data/src/handlers/__tests__/insert-typeahead.ts
@@ -338,6 +338,56 @@ describe('insert-typeahead', () => {
       ExpressionAttributeNames: {
         '#createdAt': 'createdAt',
         '#data': 'data',
+        '#descriptions': 'sales',
+        '#groupsCanAccess': 'groupsCanAccess',
+        '#owner': 'owner',
+        '#updatedAt': 'updatedAt',
+      },
+      ExpressionAttributeValues: {
+        ':data': 'owner-id:company-id:Typeahead',
+        ':descriptions': documentClient.createSet(['Description 2']),
+        ':groupsCanAccess': ['Admin'],
+        ':now': '2020-06-06T19:45:00.000Z',
+        ':owner': 'owner-id',
+      },
+      Key: {
+        __typename: 'Typeahead',
+        id: 'company-id',
+      },
+      TableName: tableName,
+      UpdateExpression:
+        'ADD #descriptions :descriptions SET #createdAt = if_not_exists(#createdAt, :now), #data = :data, #groupsCanAccess = if_not_exists(#groupsCanAccess, :groupsCanAccess), #owner = :owner, #updatedAt = :now',
+    });
+
+    expect(documentClient.update).toHaveBeenCalledWith({
+      ExpressionAttributeNames: {
+        '#createdAt': 'createdAt',
+        '#data': 'data',
+        '#descriptions': 'sales',
+        '#groupsCanAccess': 'groupsCanAccess',
+        '#owner': 'owner',
+        '#updatedAt': 'updatedAt',
+      },
+      ExpressionAttributeValues: {
+        ':data': 'owner-id:company-id:Typeahead',
+        ':descriptions': documentClient.createSet(['Description 3']),
+        ':groupsCanAccess': ['Admin'],
+        ':now': '2020-06-06T19:45:00.000Z',
+        ':owner': 'owner-id',
+      },
+      Key: {
+        __typename: 'Typeahead',
+        id: 'company-id',
+      },
+      TableName: tableName,
+      UpdateExpression:
+        'ADD #descriptions :descriptions SET #createdAt = if_not_exists(#createdAt, :now), #data = :data, #groupsCanAccess = if_not_exists(#groupsCanAccess, :groupsCanAccess), #owner = :owner, #updatedAt = :now',
+    });
+
+    expect(documentClient.update).toHaveBeenCalledWith({
+      ExpressionAttributeNames: {
+        '#createdAt': 'createdAt',
+        '#data': 'data',
         '#descriptions': 'purchases',
         '#groupsCanAccess': 'groupsCanAccess',
         '#owner': 'owner',
@@ -360,36 +410,11 @@ describe('insert-typeahead', () => {
       UpdateExpression:
         'ADD #descriptions :descriptions, #suppliers :suppliers SET #createdAt = if_not_exists(#createdAt, :now), #data = :data, #groupsCanAccess = if_not_exists(#groupsCanAccess, :groupsCanAccess), #owner = :owner, #updatedAt = :now',
     });
-
-    expect(documentClient.update).toHaveBeenCalledWith({
-      ExpressionAttributeNames: {
-        '#createdAt': 'createdAt',
-        '#data': 'data',
-        '#descriptions': 'sales',
-        '#groupsCanAccess': 'groupsCanAccess',
-        '#owner': 'owner',
-        '#updatedAt': 'updatedAt',
-      },
-      ExpressionAttributeValues: {
-        ':data': 'owner-id:company-id:Typeahead',
-        ':descriptions': documentClient.createSet(['Description 2']),
-        ':groupsCanAccess': ['Admin'],
-        ':now': '2020-06-06T19:45:00.000Z',
-        ':owner': 'owner-id',
-      },
-      Key: {
-        __typename: 'Typeahead',
-        id: 'company-id',
-      },
-      TableName: tableName,
-      UpdateExpression:
-        'ADD #descriptions :descriptions SET #createdAt = if_not_exists(#createdAt, :now), #data = :data, #groupsCanAccess = if_not_exists(#groupsCanAccess, :groupsCanAccess), #owner = :owner, #updatedAt = :now',
-    });
   });
 
   it('should call update the correct number of times', () => {
     insertTypeahead(documentClient, tableName, records);
 
-    expect(documentClient.update).toHaveBeenCalledTimes(3);
+    expect(documentClient.update).toHaveBeenCalledTimes(4);
   });
 });
