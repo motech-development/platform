3a44661e9bdbb27a73dab94571cfcee139e831ba
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-07-15 15:24:31 +0100

    chore(accounts): notification component
    affects: @accounts/client

--- applications/accounts/client/src/pages/MyCompanies/index.tsx
+++ applications/accounts/client/src/pages/MyCompanies/index.tsx
@@ -5,7 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import Apollo from '../../components/Apollo';
 import Container from '../../components/Container';
 import UserBar from '../../components/UserBar';
-import UserNotifications from '../../components/UserNotifications';
+import UserNotifications from './UserNotifications';
 
 const Accounts = lazy(() => import('./Accounts'));
 const AddCompany = lazy(() => import('./AddCompany'));
@@ -15,29 +15,6 @@ const MyCompanies = lazy(() => import('./MyCompanies'));
 const Settings = lazy(() => import('./Settings'));
 const UpdateDetails = lazy(() => import('./UpdateDetails'));
 
-// TODO: Replace with real data
-const data = {
-  getNotifications: {
-    items: [
-      {
-        createdAt: '2020-07-01T00:00:00.000Z',
-        message: 'A transaction has been published',
-        read: false,
-      },
-      {
-        createdAt: '2020-07-01T00:00:00.000Z',
-        message: 'A transaction has been published',
-        read: false,
-      },
-      {
-        createdAt: '2020-07-01T00:00:00.000Z',
-        message: 'An upload failed a virus scan',
-        read: true,
-      },
-    ],
-  },
-};
-
 const Routes: FC = () => {
   const { logout, user } = useAuth();
   const logOut = () =>
@@ -46,64 +23,55 @@ const Routes: FC = () => {
     });
 
   return (
-    <>
-      {user && data && (
+    <Apollo>
+      {user && (
         <UserBar
           name={user.name}
-          notifications={
-            <UserNotifications
-              messages={data.getNotifications.items}
-              markAsRead={async () => {
-                // TODO: Mark as read
-              }}
-            />
-          }
+          notifications={<UserNotifications id={user.sub} />}
           picture={user.picture}
           logOut={logOut}
         />
       )}
 
       <Suspense fallback={<Loader />}>
-        <Apollo>
-          <Container>
-            <Switch>
-              <ProtectedRoute
-                exact
-                component={MyCompanies}
-                path="/my-companies"
-              />
-              <ProtectedRoute
-                exact
-                component={AddCompany}
-                path="/my-companies/add-company"
-              />
-              <ProtectedRoute
-                exact
-                component={Dashboard}
-                path="/my-companies/dashboard/:companyId"
-              />
-              <Route
-                component={Accounts}
-                path="/my-companies/accounts/:companyId"
-              />
-              <Route
-                component={Clients}
-                path="/my-companies/clients/:companyId"
-              />
-              <Route
-                component={Settings}
-                path="/my-companies/settings/:companyId"
-              />
-              <ProtectedRoute
-                exact
-                component={UpdateDetails}
-                path="/my-companies/update-details/:companyId"
-              />
-            </Switch>
-          </Container>
-        </Apollo>
+        <Container>
+          <Switch>
+            <ProtectedRoute
+              exact
+              component={MyCompanies}
+              path="/my-companies"
+            />
+            <ProtectedRoute
+              exact
+              component={AddCompany}
+              path="/my-companies/add-company"
+            />
+            <ProtectedRoute
+              exact
+              component={Dashboard}
+              path="/my-companies/dashboard/:companyId"
+            />
+            <Route
+              component={Accounts}
+              path="/my-companies/accounts/:companyId"
+            />
+            <Route
+              component={Clients}
+              path="/my-companies/clients/:companyId"
+            />
+            <Route
+              component={Settings}
+              path="/my-companies/settings/:companyId"
+            />
+            <ProtectedRoute
+              exact
+              component={UpdateDetails}
+              path="/my-companies/update-details/:companyId"
+            />
+          </Switch>
+        </Container>
       </Suspense>
-    </>
+    </Apollo>
   );
 };
 
