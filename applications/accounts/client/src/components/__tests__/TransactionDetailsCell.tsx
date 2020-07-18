eeef997079bad17ccf882532afcc1357006705ea
Mo Gusbi <mo.gusbi@mo-gusbis-mbp.home>
2020-07-19 00:03:50 +0100

    chore(accounts): more tests
    affects: @accounts/client

---
+++ applications/accounts/client/src/components/__tests__/TransactionDetailsCell.tsx
@@ -0,0 +1,101 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import TransactionDetailsCell from '../TransactionDetailsCell';
+
+describe('TransactionDetailsCell', () => {
+  it('should set width on xs viewport', async () => {
+    Object.defineProperty(window, 'matchMedia', {
+      value: jest.fn().mockImplementation(query => ({
+        addEventListener: jest.fn(),
+        addListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+        matches: false,
+        media: query,
+        onchange: null,
+        removeEventListener: jest.fn(),
+        removeListener: jest.fn(),
+      })),
+      writable: true,
+    });
+
+    const { findByText } = render(
+      <table>
+        <tbody>
+          <tr>
+            <TransactionDetailsCell>Test</TransactionDetailsCell>
+          </tr>
+        </tbody>
+      </table>,
+    );
+
+    await expect(findByText('Test')).resolves.toHaveStyle(`
+      max-width: 400px;
+      min-width: 400px;
+      width: 400px;
+    `);
+  });
+
+  it('should set width on sm viewport', async () => {
+    Object.defineProperty(window, 'matchMedia', {
+      value: jest.fn().mockImplementation(query => ({
+        addEventListener: jest.fn(),
+        addListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+        matches: query === '(min-width: 576px)',
+        media: query,
+        onchange: null,
+        removeEventListener: jest.fn(),
+        removeListener: jest.fn(),
+      })),
+      writable: true,
+    });
+
+    const { findByText } = render(
+      <table>
+        <tbody>
+          <tr>
+            <TransactionDetailsCell>Test</TransactionDetailsCell>
+          </tr>
+        </tbody>
+      </table>,
+    );
+
+    await expect(findByText('Test')).resolves.toHaveStyle(`
+      max-width: 400px;
+      min-width: 400px;
+      width: 400px;
+    `);
+  });
+
+  it('should not set width on other viewports', async () => {
+    Object.defineProperty(window, 'matchMedia', {
+      value: jest.fn().mockImplementation(query => ({
+        addEventListener: jest.fn(),
+        addListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+        matches: query === '(min-width: 768px)',
+        media: query,
+        onchange: null,
+        removeEventListener: jest.fn(),
+        removeListener: jest.fn(),
+      })),
+      writable: true,
+    });
+
+    const { findByText } = render(
+      <table>
+        <tbody>
+          <tr>
+            <TransactionDetailsCell>Test</TransactionDetailsCell>
+          </tr>
+        </tbody>
+      </table>,
+    );
+
+    await expect(findByText('Test')).resolves.not.toHaveStyle(`
+      max-width: 400px;
+      min-width: 400px;
+      width: 400px;
+    `);
+  });
+});
