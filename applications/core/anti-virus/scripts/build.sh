41680105c46a71b4fbca52b9c3c1df7415f3f43d
Mo Gusbi <mo.gusbi@motechdevelopment.co.uk>
2021-12-06 11:47:50 +0000

    chore(ci): pin version on build script
    affects: @core/anti-virus

--- applications/core/anti-virus/scripts/build.sh
+++ applications/core/anti-virus/scripts/build.sh
@@ -5,7 +5,7 @@ mkdir -p clamav
 
 echo "-- Downloading AmazonLinux container --"
 docker pull amazonlinux:2018.03.0.20211015.1
-docker create -i -t -v /home/docker --name s3-antivirus-builder amazonlinux
+docker create -i -t -v /home/docker --name s3-antivirus-builder amazonlinux:2018.03.0.20211015.1
 docker start s3-antivirus-builder
 
 echo "-- Updating, downloading and unpacking clamAV and ClamAV update --"
