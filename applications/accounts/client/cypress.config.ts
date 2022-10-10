39c95821350855766b8949697c0cf5886b506ef3
Mo Gusbi <mo.gusbi@motechdevelopment.co.uk>
2022-10-10 17:03:39 +0100

    refactor: update Cypress config
    affects: @accounts/client

---
+++ applications/accounts/client/cypress.config.ts
@@ -0,0 +1,18 @@
+/* eslint-disable global-require */
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 20000,
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins')(on, config);
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+  projectId: 'Accounts',
+  video: false,
+});
