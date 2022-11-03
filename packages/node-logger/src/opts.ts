d1aaa90a3b57d5dfceaa8fd0a5993e46a69c2ef9
Mo Gusbi <mo.gusbi@motechdevelopment.co.uk>
2022-11-03 19:32:44 +0000

    refactor: use new lint config

--- packages/node-logger/src/opts.ts
+++ packages/node-logger/src/opts.ts
@@ -5,8 +5,13 @@ export const LEVEL = Symbol.for('level');
 
 export const MESSAGE = Symbol.for('message');
 
+interface IInfo {
+  [LEVEL]: string;
+  [MESSAGE]: string;
+}
+
 const opts: transports.ConsoleTransportOptions = {
-  log(info, callback) {
+  log(info: IInfo, callback) {
     setImmediate(() => (this as Transport).emit('logged', info));
 
     if (this.stderrLevels && this.stderrLevels[info[LEVEL]]) {
