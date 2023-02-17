3dab38ceaecb850e6cdc52e178c7948ac2f1fcff
Mo Gusbi <mo.gusbi@motechdevelopment.co.uk>
2023-02-17 16:52:55 +0000

    feat: link component

---
+++ packages/project-tailwind/src/components/Link.tsx
@@ -0,0 +1,71 @@
+import { ElementType } from 'react';
+import { Box, PolymorphicComponentProps } from 'react-polymorphic-box';
+import {
+  TTheme,
+  TSizing,
+  Themes,
+  Sizing,
+  useTailwind,
+} from '../utilities/tailwind';
+
+/** Default component element */
+const DEFAULT_ELEMENT = 'a';
+
+/** Link component props */
+export type TLinkProps<E extends ElementType> = PolymorphicComponentProps<
+  E,
+  {
+    /** Renders component as a block element */
+    block?: boolean;
+
+    /** Component theme */
+    theme?: TTheme;
+
+    /** Component size */
+    size?: TSizing;
+  }
+>;
+
+/**
+ * Flexible anchor element
+ *
+ * @param props - Link component props
+ *
+ * @returns Link component
+ */
+export function Link<E extends ElementType = typeof DEFAULT_ELEMENT>({
+  block = false,
+  className,
+  theme = Themes.PRIMARY,
+  size = Sizing.MD,
+  ...rest
+}: TLinkProps<E>) {
+  const { createStyles } = useTailwind(theme, size);
+
+  const linkStyles = createStyles({
+    classNames: [
+      'appearance-none bg-none border-0 font-semibold underline hover:no-underline focus:outline-none focus:ring-2 focus:ring-offset-2',
+      {
+        'block w-full text-center': block,
+        'inline-block': !block,
+      },
+      className,
+    ],
+    sizing: {
+      lg: ['text-lg'],
+      md: ['text-base'],
+      none: ['text-base'],
+      sm: ['text-sm'],
+    },
+    theme: {
+      danger: ['text-red-600 hover:text-red-700 focus:ring-red-500'],
+      primary: ['text-blue-600 hover:text-blue-700 focus:ring-blue-500'],
+      secondary: ['text-gray-600 hover:text-gray-700 focus:ring-gray-500'],
+      success: ['text-green-600 hover:text-green-700 focus:ring-green-500'],
+      warning: ['text-yellow-600 hover:text-yellow-700 focus:ring-yellow-500'],
+    },
+  });
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Box className={linkStyles} as={DEFAULT_ELEMENT} {...rest} />;
+}
---
+++ packages/project-tailwind/src/components/__tests__/Link.test.tsx
@@ -0,0 +1,61 @@
+import { render } from '@testing-library/react';
+import { ElementType } from 'react';
+import { sizing, themes } from '../../utilities/jest';
+import { Link } from '../Link';
+
+const elements = [
+  {
+    attrs: {
+      href: 'https://motech-development.github.io/platform/',
+    },
+    element: 'a' as ElementType,
+  },
+  {
+    attrs: {
+      type: 'button',
+    },
+    element: 'button' as ElementType,
+  },
+];
+
+describe('Link', () => {
+  describe.each(elements)('as element "$element"', ({ attrs, element }) => {
+    describe.each(themes)('$theme', ({ theme }) => {
+      it('should render the correct output when disabled', () => {
+        const { asFragment } = render(
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <Link disabled as={element} theme={theme} {...attrs}>
+            Hello, world
+          </Link>,
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+      });
+
+      it('should render the correct output when block element', () => {
+        const { asFragment } = render(
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <Link block as={element} theme={theme} {...attrs}>
+            Hello, world
+          </Link>,
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+      });
+
+      it.each(sizing)(
+        'should render the correct output when size is $size',
+        ({ size }) => {
+          const { asFragment } = render(
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <Link as={element} size={size} theme={theme} {...attrs}>
+              Hello, world
+            </Link>,
+          );
+
+          expect(asFragment()).toMatchSnapshot();
+        },
+      );
+    });
+  });
+});
