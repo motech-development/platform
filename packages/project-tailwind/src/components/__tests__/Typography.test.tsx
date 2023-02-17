b7f16edfebc15e1f88e9329e7b3c808811e2ddf0
Mo Gusbi <mo.gusbi@motechdevelopment.co.uk>
2023-02-17 20:35:55 +0000

    feat: typography component

---
+++ packages/project-tailwind/src/components/__tests__/Typography.test.tsx
@@ -0,0 +1,93 @@
+import { render } from '@testing-library/react';
+import { ElementType } from 'react';
+import { sizing, themes } from '../../utilities/jest';
+import { Typography } from '../Typography';
+
+const elements: ElementType[] = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'];
+
+const variants = ['body', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'lead'] as const;
+
+const alignment = ['centre', 'left', 'right'] as const;
+
+describe('Typography', () => {
+  describe.each(elements)('as element "%s"', (element) => {
+    describe.each(variants)('with the variant "%s"', (variant) => {
+      it('should render the correct output when break word is set', () => {
+        const { asFragment } = render(
+          <Typography breakWord as={element} variant={variant}>
+            Hello, world
+          </Typography>,
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+      });
+
+      it('should render the correct output when truncate is set', () => {
+        const { asFragment } = render(
+          <Typography truncate as={element} variant={variant}>
+            Hello, world
+          </Typography>,
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+      });
+
+      it.each(sizing)(
+        'should render the correct output when margin is $size',
+        ({ size }) => {
+          const { asFragment } = render(
+            <Typography as={element} margin={size} variant={variant}>
+              Hello, world
+            </Typography>,
+          );
+
+          expect(asFragment()).toMatchSnapshot();
+        },
+      );
+
+      describe.each(alignment)('with the alignment "%s"', (align) => {
+        it('should render the correct output', () => {
+          const { asFragment } = render(
+            <Typography as={element} align={align} variant={variant}>
+              Hello, world
+            </Typography>,
+          );
+
+          expect(asFragment()).toMatchSnapshot();
+        });
+
+        it('should render the correct output when rule is set', () => {
+          const { asFragment } = render(
+            <Typography rule as={element} align={align} variant={variant}>
+              Hello, world
+            </Typography>,
+          );
+
+          expect(asFragment()).toMatchSnapshot();
+        });
+      });
+
+      describe.each(themes)('with the theme "$theme"', ({ theme }) => {
+        it('should render the correct output', () => {
+          const { asFragment } = render(
+            <Typography as={element} theme={theme} variant={variant}>
+              Hello, world
+            </Typography>,
+          );
+
+          expect(asFragment()).toMatchSnapshot();
+        });
+
+        it('should render the correct output when rule is set', () => {
+          const { asFragment } = render(
+            <Typography rule as={element} theme={theme} variant={variant}>
+              Hello, world
+            </Typography>,
+          );
+
+          expect(asFragment()).toMatchSnapshot();
+        });
+      });
+    });
+  });
+});
