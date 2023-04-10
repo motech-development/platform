b3d4d3cc686fa7e796a492156dc7d4be733bc3e8
Mo Gusbi <mo.gusbi@motechdevelopment.co.uk>
2023-04-10 17:19:07 +0100

    chore: migrate from CRA to Vite

--- applications/id/client/src/i18n.ts
+++ applications/id/client/src/i18n.ts
@@ -11,7 +11,7 @@ i18n
   .use(initReactI18next)
   .init({
     backend: {
-      loadPath: `${process.env.PUBLIC_URL}/locales/{{lng}}/{{ns}}.json`,
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
     debug,
     defaultNS: 'global',
