d4f2893cfa161711a4d9400394d4b110cb7aa41a
Mo Gusbi <53015240+mogusbi-motech@users.noreply.github.com>
2023-07-13 09:24:36 +0000

    feat: set log level using env var

--- packages/node-logger/src/opts.ts
+++ packages/node-logger/src/opts.ts
@@ -6,11 +6,12 @@ export const LEVEL = Symbol.for('level');
 export const MESSAGE = Symbol.for('message');
 
 interface IInfo {
-  [LEVEL]: string;
+  [LEVEL]: number;
   [MESSAGE]: string;
 }
 
 const opts: transports.ConsoleTransportOptions = {
+  level: process.env.LOG_LEVEL ?? 'info',
   log(info: IInfo, callback) {
     setImmediate(() => (this as Transport).emit('logged', info));
 
