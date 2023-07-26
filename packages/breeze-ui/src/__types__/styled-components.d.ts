1b76fe46585a2749408b0137b98f4f1308e17051
Mo Gusbi <me@mogusbi.co.uk>
2023-07-26 20:02:01 +0000

    fix: type fixes for styled-components v6

--- packages/breeze-ui/src/__types__/styled-components.d.ts
+++ packages/breeze-ui/src/__types__/styled-components.d.ts
@@ -1,19 +1,25 @@
 import 'styled-components';
 
+interface ITheme {
+  background?: string;
+  border?: string;
+  colour?: string;
+  height?: string;
+  margin?: string;
+  padding?: string;
+  spacing?: string;
+  width?: string;
+}
+
 declare module 'styled-components' {
   export type TColour = 'primary' | 'danger' | 'secondary' | 'success';
 
   export type TSize = 'lg' | 'md' | 'none' | 'sm';
+
   export interface DefaultTheme {
-    [prop: string]: {
-      background?: string;
-      border?: string;
-      colour?: string;
-      height?: string;
-      margin?: string;
-      padding?: string;
-      spacing?: string;
-      width?: string;
-    };
+    danger: ITheme;
+    primary: ITheme;
+    secondary: ITheme;
+    success: ITheme;
   }
 }
