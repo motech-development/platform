b0821242448dbe4b69974f85a3da1b79774077e7
Mo Gusbi <me@mogusbi.co.uk>
2023-09-01 10:00:15 +0000

    feat: sentry

--- applications/accounts/client/src/index.tsx
+++ applications/accounts/client/src/index.tsx
@@ -5,36 +5,67 @@ import {
   ToastProvider,
 } from '@motech-development/breeze-ui';
 import sendToAnalytics from '@motech-development/ga-web-vitals';
-import { StrictMode } from 'react';
+import {
+  BrowserTracing,
+  init,
+  reactRouterV6Instrumentation,
+  Replay,
+} from '@sentry/react';
+import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { initialize } from 'react-ga';
-import { BrowserRouter, useNavigate } from 'react-router-dom';
+import {
+  BrowserRouter,
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigate,
+  useNavigationType,
+} from 'react-router-dom';
 import App from './App';
 import './i18n';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import invariant from './utils/invariant';
 
 const {
   REACT_APP_AUTH0_AUDIENCE,
   REACT_APP_AUTH0_CLIENT_ID,
   REACT_APP_AUTH0_DOMAIN,
   REACT_APP_GA,
+  REACT_APP_SENTRY_DSN,
 } = process.env;
 
-if (!REACT_APP_GA) {
-  throw new Error('Reporting error');
-}
+invariant(REACT_APP_SENTRY_DSN);
+
+init({
+  dsn: REACT_APP_SENTRY_DSN,
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: reactRouterV6Instrumentation(
+        useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes,
+      ),
+      tracePropagationTargets: ['localhost'],
+    }),
+    new Replay(),
+  ],
+  replaysOnErrorSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  tracesSampleRate: 1.0,
+});
+
+invariant(REACT_APP_GA);
 
 initialize(REACT_APP_GA);
 
 function Bootstrap() {
-  if (
-    !REACT_APP_AUTH0_AUDIENCE ||
-    !REACT_APP_AUTH0_CLIENT_ID ||
-    !REACT_APP_AUTH0_DOMAIN
-  ) {
-    throw new Error('Auth error');
-  }
+  invariant(REACT_APP_AUTH0_AUDIENCE);
+  invariant(REACT_APP_AUTH0_CLIENT_ID);
+  invariant(REACT_APP_AUTH0_DOMAIN);
 
   const navigate = useNavigate();
 
