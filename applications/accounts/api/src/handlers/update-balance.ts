a3f2e4bc271cfedd8189f749399af262cdcccd69
Mo Gusbi <53015240+mogusbi-motech@users.noreply.github.com>
2024-03-28 11:05:47 +0000

    test: update balance

--- applications/accounts/api/src/handlers/update-balance.ts
+++ applications/accounts/api/src/handlers/update-balance.ts
@@ -1,20 +1,12 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { AttributeValue } from '@aws-sdk/client-dynamodb';
-import { fromNodeProviderChain } from '@aws-sdk/credential-providers';
 import { unmarshall } from '@aws-sdk/util-dynamodb';
 import logger from '@motech-development/node-logger';
-import { AWSAppSyncClient } from 'aws-appsync';
 import { DynamoDBStreamHandler } from 'aws-lambda';
-import gql from 'graphql-tag';
 import { isStreamModifyRecord } from '../shared/utils';
+import updateBalance, { ITransaction } from '../shared/update-balance';
 
-interface ITransaction {
-  balance: number;
-  currency: string;
-  date: string;
-}
-
-interface IBalance {
+export interface IBalance {
   __typename: string;
   balance: number;
   id: string;
@@ -24,85 +16,6 @@ interface IBalance {
     owed: number;
     paid: number;
   };
-}
-
-export const mutation = gql`
-  mutation TransactionBeacon(
-    $id: ID!
-    $owner: String!
-    $input: TransactionBeaconInput!
-  ) {
-    transactionBeacon(id: $id, owner: $owner, input: $input) {
-      balance
-      id
-      owner
-      transactions {
-        balance
-        currency
-        date
-        items {
-          amount
-          attachment
-          category
-          companyId
-          date
-          description
-          id
-          name
-          refund
-          scheduled
-          status
-          vat
-        }
-      }
-      vat {
-        owed
-        paid
-      }
-    }
-  }
-`;
-
-interface IUpdateBalance {
-  balance?: number;
-  transactions?: ITransaction;
-  vat?: {
-    owed: number;
-    paid: number;
-  };
-}
-
-async function updateBalance(id: string, owner: string, input: IUpdateBalance) {
-  const credentials = fromNodeProviderChain({});
-
-  const { AWS_REGION, ENDPOINT } = process.env;
-
-  if (!AWS_REGION) {
-    throw new Error('No region set');
-  }
-
-  if (!ENDPOINT) {
-    throw new Error('No endpoint set');
-  }
-
-  const client = new AWSAppSyncClient({
-    auth: {
-      credentials,
-      type: 'AWS_IAM',
-    },
-    disableOffline: true,
-    region: AWS_REGION,
-    url: ENDPOINT,
-  });
-
-  return client.mutate({
-    mutation,
-    variables: {
-      id,
-      input,
-      owner,
-    },
-  });
 }
 
 export const handler: DynamoDBStreamHandler = async (event) => {
