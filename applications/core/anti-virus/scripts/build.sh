78306124ae1859e351462210a72ebdac96e0ed77
Mo Gusbi <me@mogusbi.co.uk>
2024-08-15 10:21:35 +0100

    fix: use epel mirror

--- applications/core/anti-virus/scripts/build.sh
+++ applications/core/anti-virus/scripts/build.sh
@@ -4,12 +4,12 @@ set -e
 mkdir -p clamav
 
 echo "-- Downloading AmazonLinux container --"
-docker pull amazonlinux:2023
-docker create -i -t -v /home/docker --name s3-antivirus-builder amazonlinux:2023
+docker pull amazonlinux@sha256:d8b4ba4fa093ca879d3af505f5e2083c4cfb42df3e83abf7bfad12ed208d26d2
+docker create -i -t -v /home/docker --name s3-antivirus-builder amazonlinux@sha256:d8b4ba4fa093ca879d3af505f5e2083c4cfb42df3e83abf7bfad12ed208d26d2
 docker start s3-antivirus-builder
 
 echo "-- Updating, downloading and unpacking clamAV and ClamAV update --"
-docker exec -t -w /home/docker s3-antivirus-builder yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+docker exec -t -w /home/docker s3-antivirus-builder yum install -y https://vault.centos.org/7.9.2009/extras/x86_64/Packages/epel-release-7-11.noarch.rpm
 docker exec -t -w /home/docker s3-antivirus-builder yum -y install clamav clamav-lib clamav-update json-c pcre2 libxml2 bzip2-libs libtool-ltdl xz-libs
 docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "echo 'folder content' && ls -la"
 docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "ldd /usr/bin/clamscan | grep '=>' | grep -v -e '^$' | awk '{print \$3}' | xargs -I % sh -c 'cp %* .'"
