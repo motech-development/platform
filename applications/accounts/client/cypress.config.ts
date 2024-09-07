94ab5a469ab22c694d8a7658450cf84e313ea23a
Mo Gusbi <me@mogusbi.co.uk>
2024-09-07 15:54:51 +0000

    chore: add codegen

--- applications/accounts/client/cypress.config.ts
+++ applications/accounts/client/cypress.config.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable global-require */
 import { defineConfig } from 'cypress';
 
