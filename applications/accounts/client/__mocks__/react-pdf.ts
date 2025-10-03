d743b7cc1d33f291b6e5110ae2ffd7be07051a1e
mogusbi-motech <mo.gusbi@motechdevelopment.co.uk>
2025-10-03 18:58:17 +0100

    fix: transaction form component

---
+++ applications/accounts/client/__mocks__/react-pdf.ts
@@ -0,0 +1,10 @@
+/* eslint-disable react/function-component-definition */
+export const Document = () => null;
+
+export const Page = () => null;
+
+export const pdfjs = {
+  GlobalWorkerOptions: {
+    workerSrc: '',
+  },
+};
