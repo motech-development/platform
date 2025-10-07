dbfc7e01c2989053efe79653abdcfd21dd585ea7
mogusbi-motech <mo.gusbi@motechdevelopment.co.uk>
2025-10-07 14:30:17 +0100

    feat: upgrade to eslint 9

---
+++ packages/node-logger/eslint.config.mjs
@@ -0,0 +1,9 @@
+import config from '@motech-development/eslint-config-motech-base';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  config,
+  {
+    'no-console': 'off',
+  },
+]);
