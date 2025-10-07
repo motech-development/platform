ea359ea83cdb82973d018ea42304ce20558346ec
mogusbi-motech <mo.gusbi@motechdevelopment.co.uk>
2025-10-07 17:09:33 +0100

    feat: upgrade ClamAV

--- applications/core/anti-virus/scripts/build.sh
+++ applications/core/anti-virus/scripts/build.sh
@@ -9,24 +9,70 @@ docker create -i -t -v /home/docker --name s3-antivirus-builder amazonlinux:2023
 docker start s3-antivirus-builder
 
 echo "-- Updating, downloading and unpacking ClamAV and definitions tools --"
-# AL2023 uses dnf and provides up-to-date ClamAV packages in the default repos
-docker exec -t -w /home/docker s3-antivirus-builder dnf -y update
-docker exec -t -w /home/docker s3-antivirus-builder dnf -y install \
-  clamav \
-  clamav-lib \
-  clamav-update \
-  findutils \
-  json-c \
-  pcre2 \
-  libxml2 \
-  bzip2-libs \
-  libtool-ltdl \
-  xz-libs
+# Update base and install build/runtime dependencies
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "\
+  set -eux; \
+  echo 'fastestmirror=True' >> /etc/dnf/dnf.conf; \
+  echo 'max_parallel_downloads=10' >> /etc/dnf/dnf.conf; \
+  echo 'timeout=300' >> /etc/dnf/dnf.conf; \
+  echo 'retries=10' >> /etc/dnf/dnf.conf; \
+  dnf clean all; \
+  dnf -y makecache --refresh; \
+  dnf -y update\
+"
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "\
+  set -eux; \
+  for i in 1 2 3 4 5; do \
+    dnf -y install \
+  gcc \
+  gcc-c++ \
+  make \
+  cmake \
+  ninja-build \
+  git \
+  rust \
+  cargo \
+  pkgconf-pkg-config \
+  openssl-devel \
+  zlib-devel \
+  pcre2-devel \
+  libxml2-devel \
+  bzip2-devel \
+  libcurl-devel \
+  json-c-devel \
+  ncurses-devel \
+  tar \
+  xz \
+  wget \
+  ca-certificates \
+  findutils && break || (echo 'dnf install failed, retrying...'; dnf clean all; sleep 5); \
+  done\
+"
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "update-ca-trust"
+echo "-- Building ClamAV from source (1.0.9) --"
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "\
+  set -euxo pipefail; \
+  CLAMAV_VERSION=1.0.9; \
+  CLAMAV_TAG=clamav-\${CLAMAV_VERSION}; \
+  echo \"Downloading source from GitHub releases for \${CLAMAV_VERSION}...\"; \
+  wget -q https://github.com/Cisco-Talos/clamav/releases/download/\${CLAMAV_TAG}/\${CLAMAV_TAG}.tar.gz -O \${CLAMAV_TAG}.tar.gz; \
+  echo 'Extracting...'; \
+  tar -xzf \${CLAMAV_TAG}.tar.gz; \
+  cd \${CLAMAV_TAG}; \
+  echo 'Configuring (CMake)...'; \
+  cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DENABLE_CLAMONACC=OFF -DENABLE_TESTS=OFF -DENABLE_MILTER=OFF -DENABLE_CLAMAV_MILTER=OFF -DENABLE_RAR=OFF; \
+  echo 'Building...'; \
+  cmake --build build -j; \
+  echo 'Installing...'; \
+  cmake --install build; \
+  echo 'Built ClamAV'; \
+  ls -la /usr/local/bin \
+"
 docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "echo 'folder content' && ls -la"
-docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "ldd /usr/bin/clamscan | grep '=>' | grep -v -e '^$' | awk '{print \$3}' | xargs -I % sh -c 'cp %* .'"
-docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "ldd /usr/bin/freshclam | grep '=>' | grep -v -e '^$' | awk '{print \$3}' | xargs -I % sh -c 'cp %* .'"
-docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "cp /usr/bin/clamscan ."
-docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "cp /usr/bin/freshclam ."
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "ldd /usr/local/bin/clamscan | grep '=>' | grep -v -e '^$' | awk '{print \$3}' | xargs -I % sh -c 'cp %* .'"
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "ldd /usr/local/bin/freshclam | grep '=>' | grep -v -e '^$' | awk '{print \$3}' | xargs -I % sh -c 'cp %* .'"
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "cp /usr/local/bin/clamscan ."
+docker exec -t -w /home/docker s3-antivirus-builder /bin/sh -c "cp /usr/local/bin/freshclam ."
 
 docker cp s3-antivirus-builder:/home/docker clamav
 
@@ -36,7 +82,7 @@ docker rm s3-antivirus-builder
 mkdir ./bin
 
 echo "-- Copying the executables and required libraries --"
-cp clamav/docker/* bin/.
+find clamav/docker -maxdepth 1 -type f -exec cp {} bin/. \;
 
 echo "-- Cleaning up ClamAV folder --"
 rm -rf clamav
