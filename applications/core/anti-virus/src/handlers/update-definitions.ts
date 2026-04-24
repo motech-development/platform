bb65860dbc352c886c8085519bfe624e42421f66
Mo Gusbi <mo.gusbi@motechdevelopment.co.uk>
2026-04-25 00:58:24 +0100

    fix: sonarqube code smells

--- applications/core/anti-virus/src/handlers/update-definitions.ts
+++ applications/core/anti-virus/src/handlers/update-definitions.ts
@@ -12,8 +12,8 @@ init({
   dsn: process.env.SENTRY_DSN,
   integrations: [nodeProfilingIntegration()],
   profileLifecycle: 'trace',
-  profileSessionSampleRate: 1.0,
-  tracesSampleRate: 1.0,
+  profileSessionSampleRate: 1,
+  tracesSampleRate: 1,
 });
 
 export const handler: Handler = wrapHandler(async () => {
