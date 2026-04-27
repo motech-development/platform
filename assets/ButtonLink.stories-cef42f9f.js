928d27efd0b22247064a49b6b18a779a5f1b4486
mogusbi-motech <mogusbi-motech@users.noreply.github.com>
2026-04-27 20:37:51 +0000

    deploy: 6dc959b6195177edb841c0025af958bc0e8bf3d2

---
+++ assets/ButtonLink.stories-cef42f9f.js
@@ -0,0 +1,10 @@
+import{j as t,B as d}from"./BaseStyles-5cc577b5.js";import{d as i}from"./index-d8a6010f.js";import{C as m}from"./Card-910ce953.js";import{B as p}from"./BaseLink-5ada74d4.js";import"./index-cb576d23.js";import"./_commonjsHelpers-de833af9.js";const s=o=>t.jsx(p,{as:"button",...o}),c=s;try{s.displayName="ButtonLink",s.__docgenInfo={description:"",displayName:"ButtonLink",props:{}}}catch{}const L={component:c,decorators:[i.withKnobs]},n={name:"Basic button link",render:()=>t.jsxs(t.Fragment,{children:[t.jsx(d,{}),t.jsx(m,{children:t.jsx(c,{children:i.text("Text","This is a button")})})]})};var r,e,a;n.parameters={...n.parameters,docs:{...(r=n.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Basic button link',
+  render: () => <>
+      <BaseStyles />
+
+      <Card>
+        <ButtonLink>{text('Text', 'This is a button')}</ButtonLink>
+      </Card>
+    </>
+}`,...(a=(e=n.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const h=["BasicButtonLink"];export{n as BasicButtonLink,h as __namedExportsOrder,L as default};
---
+++ assets/Card-910ce953.js
@@ -0,0 +1,7 @@
+import{s as r,j as n,L as p}from"./BaseStyles-5cc577b5.js";const s={lg:{padding:"20px"},md:{padding:"10px"},none:{padding:"0"},sm:{padding:"5px"}},i=r.div`
+  ${({$padding:d,theme:a})=>`
+    background: #f8f8f8;
+    color: #000;
+    padding: ${a[d].padding}
+  `}
+`,e=({children:d,padding:a="md"})=>n.jsx(p,{theme:s,children:n.jsx(i,{$padding:a,children:d})}),t=e;try{e.displayName="Card",e.__docgenInfo={description:"",displayName:"Card",props:{padding:{defaultValue:{value:"md"},description:"",name:"padding",required:!1,type:{name:"enum",value:[{value:'"none"'},{value:'"sm"'},{value:'"md"'},{value:'"lg"'}]}}}}}catch{}export{t as C};
